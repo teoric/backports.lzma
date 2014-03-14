754f5e9e91fe801ea62875b718a17813917944de
Tomer Chachamu <tomer@gambitresearch.com>
2014-03-14 15:15:29 +0000

    bump version

--- lzmaffi/__init__.py
+++ lzmaffi/__init__.py
@@ -37,7 +37,7 @@ _MODE_READ_EOF = 2
 _MODE_WRITE    = 3
 
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 class _LZMAFile(io.BufferedIOBase):
     """A file object providing transparent LZMA (de)compression.
